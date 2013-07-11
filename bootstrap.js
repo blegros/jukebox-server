8a30ef9581f01e0419a89ab7d45f56caa3968435
Brian LeGros <me@brianlegros.com>
2013-07-11 02:01:12 -0400

    Added nodefly integration.

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,11 @@
+//Hook to integrate nodefly
+if (process.env.NODE_ENV === 'production') {
+    require('nodefly').profile(
+        'cf31db9a-3150-4e63-a2fe-a8709903b657',
+        ['open-jukebox-server', 'Heroku']
+    );
+}
+
 var server = require('./app')();
 
 // start server
